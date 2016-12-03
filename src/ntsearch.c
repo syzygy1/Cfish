9a6170388381d0839682a531d06802dfe1fe56c5
syzygy <syzygy@server.fake>
2016-12-03 01:51:16 +0100

    Updated to "Simplify pruning rule". https://github.com/official-stockfish/Stockfish/commit/e70da0d2eb20cffee505c4e7ef61399de7d3fa16

--- src/ntsearch.c
+++ src/ntsearch.c
@@ -428,9 +428,10 @@ moves_loop: // When in check search starts from here.
 //      else if (   depth < 7 * ONE_PLY && ss->stage != ST_GOOD_CAPTURES
 //               && !see_test(pos, move, -35 * depth / ONE_PLY * depth / ONE_PLY))
       else if (depth < 7 * ONE_PLY && !extension) {
-        Value v = -35 * depth / ONE_PLY * depth / ONE_PLY;
-        if (ss->staticEval != VALUE_NONE)
-          v += ss->staticEval - alpha - 200;
+        Value v = -(399 + 35 * depth / ONE_PLY * depth / ONE_PLY);
+
+        if (PvNode)
+          v += beta - alpha - 1;
 
         if (!see_test(pos, move, v))
           continue;
