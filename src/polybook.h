dba796faf3f647a82c83ae33c676c74626861569
syzygy <syzygy@server.fake>
2017-12-11 21:17:25 +0100

    Missing files for Cerebellum support. Forgot to add polybook.c and polybook.h.

---
+++ src/polybook.h
@@ -0,0 +1,16 @@
+/* polybook.h from BrainFish, Copyright (C) 2016-2017 Thomas Zipproth */
+
+#ifndef POLYBOOK_H_INCLUDED
+#define POLYBOOK_H_INCLUDED
+
+#include "bitboard.h"
+#include "position.h"
+
+void pb_init(const char *bookfile);
+void pb_set_best_book_move(int best_book_move);
+void pb_set_book_depth(int book_depth);
+
+Move pb_probe(Pos *pos);
+
+#endif
+
