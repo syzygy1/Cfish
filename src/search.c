3505849a6017dfae1c5e4bda797a6d79c8fa79a0
syzygy1 <3028851+syzygy1@users.noreply.github.com>
2018-03-28 21:47:54 +0200

    Updated to "Change reduction/improving interaction". https://github.com/official-stockfish/Stockfish/commit/9953bfff621b287f5e08139f51d6663c2620db1e

--- src/search.c
+++ src/search.c
@@ -131,7 +131,7 @@ void search_init(void)
         Reductions[PV][imp][d][mc] = max(Reductions[NonPV][imp][d][mc] - 1, 0);
 
         // Increase reduction for non-PV nodes when eval is not improving
-        if (!imp && Reductions[NonPV][imp][d][mc] >= 2)
+        if (!imp && r > 1.0)
           Reductions[NonPV][imp][d][mc]++;
       }
 
