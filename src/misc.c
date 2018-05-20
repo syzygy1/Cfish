57094aa4d784b9ed4a37429972b42669ae5282b6
syzygy1 <3028851+syzygy1@users.noreply.github.com>
2018-05-20 12:22:55 +0200

    Fix compilation error. Fixes https://github.com/syzygy1/Cfish/issues/61

--- src/misc.c
+++ src/misc.c
@@ -197,7 +197,7 @@ size_t file_size(FD fd)
   return statbuf.st_size;
 #else
   DWORD sizeLow, sizeHigh;
-  size_low = GetFileSize(fd, &sizeHigh);
+  sizeLow = GetFileSize(fd, &sizeHigh);
   return ((size_t)sizeHigh << 32) | (size_t)sizeLow;
 #endif
 }
