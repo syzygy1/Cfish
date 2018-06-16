94172b34be585fa4b738a7ee3b0b6c4785b5fbe9
syzygy1 <3028851+syzygy1@users.noreply.github.com>
2018-06-16 16:13:47 +0200

    Updated to "Remove lmrDepth restriction on quiet see pruning". https://github.com/official-stockfish/Stockfish/commit/a834bfe8332adcb0dfc1fd280f1f9d8bbce86266

--- src/ntsearch.c
+++ src/ntsearch.c
@@ -496,15 +496,13 @@ moves_loop: // When in check search starts from here.
 
         // Prune moves with negative SEE at low depths and below a decreasing
         // threshold at higher depths.
-        if (   lmrDepth < 8
-            && !extension
-            && !see_test(pos, move, -35 * lmrDepth * lmrDepth))
+        if (   !extension
+            && !see_test(pos, move, -29 * lmrDepth * lmrDepth))
           continue;
       }
 //      else if (   depth < 7 * ONE_PLY && ss->stage != ST_GOOD_CAPTURES
 //               && !see_test(pos, move, -35 * depth / ONE_PLY * depth / ONE_PLY))
-      else if (    depth < 7 * ONE_PLY
-               && !extension
+      else if (   !extension
                && !see_test(pos, move, -PawnValueEg * (depth / ONE_PLY)))
         continue;
     }
