80f2549ce11dde12ff8aa3791608ced11201b1ac
syzygy1 <3028851+syzygy1@users.noreply.github.com>
2020-06-25 02:10:48 +0200

    Slight reformatting.

--- src/avx2-bitboard.c
+++ src/avx2-bitboard.c
@@ -15,12 +15,12 @@ static void init_sliding_attacks(void)
   for (sq = SQ_A1; sq <= SQ_H8; ++sq)
     for (j = 0; j < 2; ++j) {
       for (i = 0; i < 4; ++i) {
-          attacks[i] = 0;
-          for (s = sq + dirs[j][i];
-               square_is_ok(s) && distance(s, s - dirs[j][i]) == 1; s += dirs[j][i])
-          {
-              attacks[i] |= sq_bb(s);
-          }
+        attacks[i] = 0;
+        for (s = sq + dirs[j][i];
+            square_is_ok(s) && distance(s, s - dirs[j][i]) == 1; s += dirs[j][i])
+        {
+          attacks[i] |= sq_bb(s);
+        }
       }
       queen_mask_v4[sq][j] = _mm256_set_epi64x(attacks[3], attacks[2], attacks[1], attacks[0]);
     }
@@ -28,13 +28,13 @@ static void init_sliding_attacks(void)
   // pseudo attacks for Rook (NORTH-SOUTH) and Bishop
   for (sq = SQ_A1; sq <= SQ_H8; ++sq) {
     rook_mask_NS[sq] = _mm_set_epi64x(
-      _mm256_extract_epi64(queen_mask_v4[SQUARE_FLIP(sq)][0], 1),	// SOUTH (vertically flipped)
-      _mm256_extract_epi64(queen_mask_v4[sq][0], 1));	// NORTH
+        _mm256_extract_epi64(queen_mask_v4[SQUARE_FLIP(sq)][0], 1),	// SOUTH (vertically flipped)
+        _mm256_extract_epi64(queen_mask_v4[sq][0], 1));	// NORTH
     bishop_mask_v4[sq] = _mm256_set_epi64x(
-      _mm256_extract_epi64(queen_mask_v4[SQUARE_FLIP(sq)][0], 2),	// SOUTH_EAST (vertically flipped)
-      _mm256_extract_epi64(queen_mask_v4[sq][0], 3),	// NORTH_WEST
-      _mm256_extract_epi64(queen_mask_v4[SQUARE_FLIP(sq)][0], 3),	// SOUTH_WEST (vertically flipped)
-      _mm256_extract_epi64(queen_mask_v4[sq][0], 2));	// NORTH_EAST
+        _mm256_extract_epi64(queen_mask_v4[SQUARE_FLIP(sq)][0], 2),	// SOUTH_EAST (vertically flipped)
+        _mm256_extract_epi64(queen_mask_v4[sq][0], 3),	// NORTH_WEST
+        _mm256_extract_epi64(queen_mask_v4[SQUARE_FLIP(sq)][0], 3),	// SOUTH_WEST (vertically flipped)
+        _mm256_extract_epi64(queen_mask_v4[sq][0], 2));	// NORTH_EAST
   }
 
   // sliding attacks for Rook EAST-WEST
@@ -42,12 +42,12 @@ static void init_sliding_attacks(void)
     for (sq = 0; sq < 8; ++sq) {
       att8 = 0;
       for (s8 = (1 << sq) << 1; s8; s8 <<= 1) {
-	att8 |= s8;
+        att8 |= s8;
         if (occ8 & s8)
           break;
       }
       for (s8 = (1 << sq) >> 1; s8; s8 >>= 1) {
-	att8 |= s8;
+        att8 |= s8;
         if (occ8 & s8)
           break;
       }
