43390774ca93677ff49e276cef98f6ec5b9d11e0
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-07-12 14:13:28 +0200

    Update dense implementation to "New NNUE architecture and net" https://github.com/official-stockfish/Stockfish/commit/e8d64af1230fdac65bb0da246df3e7abe82e0838

--- src/nnue.h
+++ src/nnue.h
@@ -9,7 +9,8 @@
 enum { ACC_EMPTY, ACC_COMPUTED, ACC_INIT };
 
 typedef struct {
-  alignas(64) int16_t accumulation[2][256];
+  alignas(64) int16_t accumulation[2][512];
+  alignas(64) int32_t accumulation_psqt[2][8];
   uint8_t state[2];
 } Accumulator;
 
